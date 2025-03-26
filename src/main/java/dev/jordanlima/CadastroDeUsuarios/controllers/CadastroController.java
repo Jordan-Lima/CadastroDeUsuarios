91abff47418a04f148eede1edfc22c57815f26cc
Jordan Lima <jordanlimasuhett@gmail.com>
2025-03-26 10:17:39 -0300

    Criação dos controllers

---
+++ src/main/java/dev/jordanlima/CadastroDeUsuarios/controllers/CadastroController.java
@@ -0,0 +1,23 @@
+package dev.jordanlima.CadastroDeUsuarios.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CadastroController {
+    @GetMapping("/register")
+    public String cadastroResponse() {
+        return "<h1 align='center'>Cadastro</h1>";
+    }
+    @GetMapping("/users")
+    public String usuariosResponse() {
+        return "<h1 align='center'>Usuários</h1>";
+    }
+    @GetMapping("/login")
+    public String loginResponse() {
+        return "<h1 align='center'>Login</h1>";
+    }
+}
