3878849a67cd1750ee503882bf6a93c449257c9f
Jordan Lima <jordanlimasuhett@gmail.com>
2025-03-26 13:21:50 -0300

    adição do lombok e arrumando o erro de UsuarioModel está como lista

--- src/main/java/dev/jordanlima/CadastroDeUsuarios/dispositivos/DispositivoModel.java
+++ src/main/java/dev/jordanlima/CadastroDeUsuarios/dispositivos/DispositivoModel.java
@@ -2,11 +2,15 @@ package dev.jordanlima.CadastroDeUsuarios.dispositivos;
 
 import dev.jordanlima.CadastroDeUsuarios.usuarios.UsuarioModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "TB_DEVICE")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class DispositivoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +20,5 @@ public class DispositivoModel {
 
     @ManyToOne
     @JoinColumn(name = "usuario_id")
-    private List<UsuarioModel> usuario;
+    private UsuarioModel usuario;
 }
