e8314a173f1fd572d31ec6ed367f404cc9f3f4e1
Jordan Lima <jordanlimasuhett@gmail.com>
2025-03-26 12:59:58 -0300

    Adição da camada de administradores, ainda não codada

---
+++ src/main/java/dev/jordanlima/CadastroDeUsuarios/administradores/AdminController.java
@@ -0,0 +1,4 @@
+//package dev.jordanlima.CadastroDeUsuarios.administradores;
+//
+//public class AdminController {
+//}
---
+++ src/main/java/dev/jordanlima/CadastroDeUsuarios/administradores/AdminModel.java
@@ -0,0 +1,47 @@
+//package dev.jordanlima.CadastroDeUsuarios.administradores;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "TB_ADMIN")
+//public class AdminModel {
+//    private String name;
+//    private String email;
+//    private String password;
+//
+//    public AdminModel() {
+//    }
+//
+//    public AdminModel(String name, String email, String password) {
+//        this.name = name;
+//        this.email = email;
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
